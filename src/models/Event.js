a1f366a239c02e627ea8f927a13488dfe0c340b4
Omar Al-Ezzi <ishtaar.it@gmail.com>
2022-02-07 18:09:10 +0100

    add RegistrationController, RegistrationController,RegistrationController and Registration ....BACKEND v0.3.1

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,22 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const EventSchema = new mongoose.Schema({
+const EventSchema = new mongoose.Schema(
+  {
     title: String,
     description: String,
     price: Number,
     thumbnail: String,
-    category: String, 
+    spocategory: String,
     date: Date,
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
-}, {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
     toJSON: {
-        virtuals: true
-    }
+      virtuals: true,
+    },
+  }
+);
+
+EventSchema.virtual("thumbnail_url").get(function () {
+  return `http://localhost:8000/files/${this.thumbnail}`;
 });
 
-EventSchema.virtual("thumbnail_url").get(function () { return `http://localhost:8000/files/${this.thumbnail}` })
-
-module.exports = mongoose.model('Event', EventSchema)
+module.exports = mongoose.model("Event", EventSchema);
--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,51 @@
-const express = require('express')
-const multer = require('multer')
+const express = require("express");
+const multer = require("multer");
 
-const UserController = require('./controllers/UserController')
-const EventController = require('./controllers/EventController')
-const uploadConfig = require('./config/upload')
+const UserController = require("./controllers/UserController");
+const EventController = require("./controllers/EventController");
+const DashboardController = require("./controllers/DashboardController");
+const LoginController = require("./controllers/LoginController");
+const RegistrationController = require("./controllers/RegistrationController");
+const ApprovalController = require("./controllers/ApprovalController");
+const RejectionController = require("./controllers/RejectionController");
+const uploadConfig = require("./config/upload");
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
-routes.get('/status', (req, res) => {
-    res.send({ status: 200 })
-})
+routes.get("/status", (req, res) => {
+  res.send({ status: 200 });
+});
 
+//Registration
+routes.post("/registration/:eventId", RegistrationController.create);
+routes.get(
+  "/registration/:registration_id",
+  RegistrationController.getRegistration
+);
+routes.post(
+  "/registration/:registration_id/approvals",
+  ApprovalController.approval
+);
+routes.post(
+  "/registration/:registration_id/rejections",
+  RejectionController.rejection
+);
 
-//Event
-routes.get('/event/:eventId', EventController.getEventById)
-routes.post('/event', upload.single("thumbnail"), EventController.createEvent)
+//Login
+routes.post("/login", LoginController.store);
 
+//Dashboard
+routes.get("/dashboard/:category", DashboardController.getAllEvents);
+routes.get("/dashboard", DashboardController.getAllEvents);
+routes.get("/event/:eventId", DashboardController.getEventById);
+
+//Events
+routes.post("/event", upload.single("thumbnail"), EventController.createEvent);
+routes.delete("/event/:eventId", EventController.delete);
 
 //User
-routes.post('/user/register', UserController.createUser)
-routes.get('/user/:userId', UserController.getUserById)
-
+routes.post("/user/register", UserController.createUser);
+routes.get("/user/:userId", UserController.getUserById);
 
 module.exports = routes;
